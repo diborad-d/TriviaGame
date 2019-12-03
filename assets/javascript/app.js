d623307d05e88db377e649254c9d913203767a90
Dibora D <diborad@yahoo.com>
2019-12-03 17:03:55 -0600

    set timer div added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,64 @@
-const questionOne = {
-  question: "Who was the legendary Benedictine monk who invented champagne?",
-  choices: ["Dom Perignon", "Henry Hugh", "Walter William", "Beatrice Richard "],
-  correctAnswer: 0
-};
-const questionTwo = {
-    question: "What's the largest freshwater lake in the world?",
-    choices: ["Lake Huron", "Lake Superior", "Lake Victoria", "Lake Michigan "],
-    correctAnswer: 1
-  };
-  const questionThree = {
-    question: "Where would you find the Sea of Tranquility?",
-    choices: ["Mars", "Earth", "The Mooon", "Mecuriy"],
-    correctAnswer: 2  
-  };
-  const questionFour = {
-    question: "Who was the legendary Benedictine monk who invented champagne?",
-    choices: ["Dom Perignon", "Henry Hugh", "Walter William", "Beatrice Richard "],
-    correctAnswer: 0
-  };
-  const questionFive = {
-    question: "What kind of weapon is a falchion? ",
-    choices: ["Revolver", "Crossbow", "Spear", "Sword"],
-    correctAnswer: 3
-  };
-//   const questionSix = {
-//     question: "What is another word for lexicon?",
-//     answers: ["Dom Perignon", "Henry Hugh", "Walter William", "Beatrice Richard "],
-//     correctAnswer: 0
-//   };
-//   const questionSeven = {
-//     question: "Who was the legendary Benedictine monk who invented champagne?",
-//     answers: ["Dom Perignon", "Henry Hugh", "Walter William", "Beatrice Richard "],
-//     correctAnswer: 0
-//   };
-//   const questionEight = {
-//     question: "Who was the legendary Benedictine monk who invented champagne?",
-//     answers: ["Dom Perignon", "Henry Hugh", "Walter William", "Beatrice Richard "],
-//     correctAnswer: 0
-//   };
-// let currentQuestion = randomIndex();
+let currentQuestionIndex = 0;
+const questions = [
+  {
+    questionText: "Who was the legendary Benedictine monk who invented champagne?",
+    choiceA: "Dom Perignon",
+    choiceB: "Henry Hugh",
+    choiceC: "Walter William",
+    choiceD: "Beatrice Richard ",
+    correctAnswer: "A"
+  },
+  {
+    questionText: "What's the largest freshwater lake in the world?",
+    choiceA: "Lake Huron",
+    choiceB: "Lake Superior",
+    choiceC: "Lake Victoria",
+    choiceD: "Lake Michigan ",
+    correctAnswer: "B"
+  }
+];
 window.onload = function() {
-  setup();
+  $("#radioA").click(function() {
+    radioBtnChecked("A");
+    this.checked = false;
+  });
+  $("#radioB").click(function() {
+    radioBtnChecked("B");
+    this.checked = false;
+  });
+  $("#radioC").click(function() {
+    radioBtnChecked("C");
+    this.checked = false;
+  });
+  $("#radioD").click(function() {
+    radioBtnChecked("D");
+    this.checked = false;
+  });
+
+  //   $("#submit").click(submitClicked); /* setting parameter(function) called submitClick and passing it as a variable to the on click event */
+
+  showNextQuestion();
 };
-function setup() {
-  $("#currentQuestioin").text(questionOne.question);
-  $("#optionA").text(questionOne.choices[0]);
-  $("#optionB").text(questionOne.choices[1]);
-  $("#optionC").text(questionOne.choices[2]);
-  $("#optionD").text(questionOne.choices[3]) ;
+function showNextQuestion() {
+  showQuestion(questions[currentQuestionIndex]);
 }
- 
+function showQuestion(question) {
+  $("#question").text(question.questionText);
+  $("#choiceA").text(question.choiceA);
+  $("#choiceB").text(question.choiceB);
+  $("#choiceC").text(question.choiceC);
+  $("#choiceD").text(question.choiceD);
+}
+function radioBtnChecked(choice) {
+  const correctAnswer = questions[currentQuestionIndex].correctAnswer;
+  if (choice === correctAnswer) {
+   console.log("Correct");
+  } if (choice !== correctAnswer)
+  console.log("Wrong!");
+  alert("The correct Answer is " + correctAnswer);
+  currentQuestionIndex++;
+  showNextQuestion();
+}
+function submitClicked() {
+  console.log("submit");
+}
